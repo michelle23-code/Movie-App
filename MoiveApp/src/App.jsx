c7ef2195e577f7327fa8e8873f66dab3c64a4fdd
michelle23-code <michellelekoto13@icloud.com>
2024-12-20 01:50:17 +0200

    added api

---
+++ MoiveApp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+  const url ="http://www.omdbapi.com/?apikey=340bedb7&";
+
+  return ( 
+  <h1>This is our app compenent </h1>
+
+  )
+
+};
+export default App;
