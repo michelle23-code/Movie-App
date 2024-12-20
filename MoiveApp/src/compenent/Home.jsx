979b84a40f168afd0ed4d0885c2cc1db9a26e45a
michelle23-code <michellelekoto13@icloud.com>
2024-12-20 02:17:38 +0200

    updated index

--- MoiveApp/src/compenent/Home.jsx
+++ MoiveApp/src/compenent/Home.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+const Home = () =>{
+    return(
+        <>
+         <h1>This is Home page.</h1>
+        </>
+    )
+};
+
+export default Home;
---
+++ MoiveApp/src/compenent/Single.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Single = () =>{
+    return(
+        <h1>This a single compant</h1>
+    )
+}
